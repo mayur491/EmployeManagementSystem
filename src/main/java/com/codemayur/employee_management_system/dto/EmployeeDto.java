751843bd19c25eec8882ab4225ef0c97c4ac2d84
Mayur Somani <mayur491@gmail.com>
2022-01-29 23:48:18 +0530

    removing illegal character: '\'

--- src/main/java/com/codemayur/employee_management_system/dto/EmployeeDto.java
+++ src/main/java/com/codemayur/employee_management_system/dto/EmployeeDto.java
@@ -8,7 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;\
+import javax.validation.constraints.NotNull;
 
 /**
  * @author mayur.somani
